6e6ed0875d55df38061152235a481ed7d3ac4bd8
marechal <gerald1973@gmail.com>
2016-01-14 17:13:33 +0100

    ...

--- LabelPrinter/src/main/java/com/smilesmile1973/ms/word/PageSetup.java
+++ LabelPrinter/src/main/java/com/smilesmile1973/ms/word/PageSetup.java
@@ -4,18 +4,25 @@ import org.eclipse.swt.ole.win32.Variant;
 
 import com.smilesmile1973.OleUtils;
 
+/**
+ * This class refers the PageSetup bject of the Document.
+ * 
+ * @author marechal
+ */
 public class PageSetup extends AbstractOleWordObject<Document> {
 
 	public PageSetup(Document parent, Variant myVariant) {
 		super(parent, myVariant);
 	}
-	
+
 	/**
 	 * This method set up the page.
-	 * @param paperSize a constant WdPaperSize
+	 * 
+	 * @param paperSize
+	 *            a constant WdPaperSize
 	 * @author marechal
 	 */
-	public void setPaperSize(int paperSize){
+	public void setPaperSize(int paperSize) {
 		OleUtils.INSTANCE.setProperty(getMyVariant().getAutomation(), "PaperSize", new Variant(paperSize));
 	}
 
